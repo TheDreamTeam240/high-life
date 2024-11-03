f9b366f41e3b398beaf3e8623d6145a585077a47
MathTek <mathieu.rasoanaivo@epitech.eu>
2024-11-03 13:47:56 +0400

    Merge main into 12

---
+++ scenes/dependency_bar.gd
@@ -0,0 +1,29 @@
+#extends Control
+#
+#@onready var rich_text_label: RichTextLabel = $RichTextLabel
+#
+#var messages: Array = [
+	#"Café",
+	#"Codéine",
+	#"Test",
+	#"Test2",
+	#"Test3"
+#]
+#var current_index: int = 0  # Déclaration comme variable d'instance
+#
+#func _ready() -> void:    
+	## Met à jour les messages au démarrage
+	#update_messages()
+#
+## Appelé chaque fois qu'un enfant est ajouté
+#func _on_child_added(_child: Node) -> void:
+	#update_messages()
+#
+## Met à jour le RichTextLabel avec le message suivant
+#func update_messages() -> void:
+	#if current_index < messages.size():  # Vérifie s'il y a encore des messages à afficher
+		#rich_text_label.add_text(messages[current_index] + "\n")  # Ajouter le message au RichTextLabel
+		#current_index += 1  # Incrémente l'index pour le prochain message
+#
+#func _process(_delta: float) -> void:
+	#pass
--- scripts/player_action.gd
+++ scripts/player_action.gd
@@ -1,19 +1,29 @@
 extends CharacterBody3D
+
 @onready var visuals = $visuals
 @onready var animate_player =$visuals/playerv1/AnimationPlayer
 @onready var camera_mount =$camera_mount
 @onready var container = $Inventory/InventoryWindow
 @onready var player = $"."
-#@onready var poudre = $SlotContainer/Slot1
-#@onready var cana = $SlotContainer/Slot2
-#@onready var bottle = $SlotContainer/Slot3
-#@onready var cig = $SlotContainer/Slot4
-#@onready var cofee = $SlotContainer/Slot5
 
+@onready var rich_text_labels: Array = [
+	$"Dependency bar/RichTextLabel",
+	$"Dependency bar2/RichTextLabel",
+	$"Dependency bar3/RichTextLabel",
+	$"Dependency bar4/RichTextLabel",
+	$"Dependency bar5/RichTextLabel"
+]
 
+@onready var progress_bar: Array = [
+	$"Dependency bar/TextureProgressBar",
+	$"Dependency bar2/TextureProgressBar",
+	$"Dependency bar3/TextureProgressBar",
+	$"Dependency bar4/TextureProgressBar",
+	$"Dependency bar5/TextureProgressBar"
+]
 
 var SPEED = 2.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 2.5
 
 var walking_speed = 0.6
 var running_speed = 3.0
@@ -21,14 +31,24 @@ var running_speed = 3.0
 var running = false
 var is_locked = false
 
+var messages: Array = [
+	"Café",
+	"Alcool",
+	"Cigarette",
+	"Zamal",
+	"Cocaine"
+]
+var current_index: int = 0
+
 @export var sens_horizontal = 0.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _ready():
+	
+func _ready() -> void:
 	container.visible = false
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
+	update_messages()
 	
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -38,8 +58,8 @@ func _input(event):
 			visuals.rotate_y(deg_to_rad(event.relative.x*sens_horizontal))
 
 
+
 func _physics_process(delta: float):
-	
 	if Input.is_action_just_pressed("inventory"):
 		container.visible = !container.visible
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -53,7 +73,6 @@ func _physics_process(delta: float):
 	
 	if !animate_player.is_playing():
 		is_locked =  false
-		
 	
 	if Input.is_action_just_pressed("kick"):
 		if !container.visible:
@@ -68,16 +87,12 @@ func _physics_process(delta: float):
 		SPEED = walking_speed
 		running = false
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("right", "left", "back", "forward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -88,8 +103,8 @@ func _physics_process(delta: float):
 			else:
 				if animate_player.current_animation != "walk_normal":
 					animate_player.play("walk_normal")
-	 
-			visuals.look_at(position + direction *-1)
+		
+			visuals.look_at(position + direction * -1)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
@@ -101,3 +116,22 @@ func _physics_process(delta: float):
 	
 	if !is_locked:
 		move_and_slide()
+
+func _on_child_added(_child: Node) -> void:
+	update_messages()
+
+func update_messages() -> void:
+	if current_index < messages.size():
+		for label in rich_text_labels:
+			label.add_text(messages[current_index] + "\n")
+			current_index += 1
+
+func _process(delta: float) -> void:
+	for bar in progress_bar:
+		bar.value -= 0.4
+
+func _on_dealer_body_entered(body: Node3D) -> void:
+	get_tree().change_scene_to_file("res://Dealer.tscn")
+
+func _on_shop_body_entered(body: Node3D) -> void:
+	get_tree().change_scene_to_file("res://Shop.tscn")
